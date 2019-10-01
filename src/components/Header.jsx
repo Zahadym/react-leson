fe980fc0b8780bf23c015d0d52185713f0ad0fbc
Zahadym <47822127+Zahadym@users.noreply.github.com>
2019-10-01 13:09:57 +0400

    start my repository

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvfbTs4ebgJIjXtdulcwJPij1NFWDKJYHP4RvWAIk-6E8uea61'/>
+        </header>
+    );
+}
+
+export default Header;
